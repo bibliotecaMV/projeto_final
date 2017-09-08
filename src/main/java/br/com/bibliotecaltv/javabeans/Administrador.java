e6ae857411303f7fed2aa9e93eea26b377325e22
Pedro Lucas <pedro.matos@MVBRFORDSK7683.MVFOR.local>
2017-09-08 17:30:13 -0300

    Padrao de projeto com o Livro e o ADM feito pelo hibernate

---
+++ src/main/java/br/com/bibliotecaltv/javabeans/Administrador.java
@@ -0,0 +1,40 @@
+package br.com.bibliotecaltv.javabeans;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Administradores")
+public class Administrador {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(nullable = false)
+	private String usuario;
+	@Column(nullable = false)
+	private String senha;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
