6efbe8390dbecf320c782cc2f10c0198c3d0c692
Renato Richard <Renato Richard>
2017-09-11 14:35:52 -0300

    tabela aluno, professor e monitor adicionada.

--- src/main/java/br/com/bibliotecaltv/javabeans/Professor.java
+++ src/main/java/br/com/bibliotecaltv/javabeans/Professor.java
@@ -1,5 +1,39 @@
 package br.com.bibliotecaltv.javabeans;
 
-public class Professor {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="professores")
+public class Professor {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(name="nome_completo")
+	private String nomeCompleto;
+	@Column(name="matricula")
+	private Long matricula;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+	public Long getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(Long matricula) {
+		this.matricula = matricula;
+	}
+	
 }
