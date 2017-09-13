1ea8bf171bfa5288e0eecc4dabedd965700857c0
Pedro Lucas <pedro.matos@MVFOR.local>
2017-09-13 15:46:48 -0300

    Função de inserirAdministrador e inserirMonitor com seus devidos testes

--- src/main/java/br/com/bibliotecaltv/dao/AdministradorDAO.java
+++ src/main/java/br/com/bibliotecaltv/dao/AdministradorDAO.java
@@ -1,8 +1,40 @@
 package br.com.bibliotecaltv.dao;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
+
+import br.com.bibliotecaltv.controller.javabeans.Administrador;
+import br.com.bibliotecaltv.controller.javabeans.Monitores;
+import br.com.bibliotecaltv.sessaoHibernate.HibernateUtil;
 
 @Repository
 public class AdministradorDAO {
 	
+	public void adicionarAdministrador(Administrador administrador){
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction transaction = null;
+		try{
+			transaction = session.beginTransaction();
+			session.save(administrador);
+			transaction.commit();
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			session.close();
+		}	
+	}
+	public void adicionarMonitor(Monitores monitores){
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction transaction = null;
+		try{
+			transaction = session.beginTransaction();
+			session.save(monitores);
+			transaction.commit();
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			session.close();
+		}	
+	}
 }
