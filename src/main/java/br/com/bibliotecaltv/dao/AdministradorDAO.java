37709dd499d1795f90638e65b3caccd8e83e5b91
Carla Valéria <carla_empresa@outlook.com>
2017-09-13 15:48:17 -0300

    adicionando a preparação dos métodos de excluir

--- src/main/java/br/com/bibliotecaltv/dao/AdministradorDAO.java
+++ src/main/java/br/com/bibliotecaltv/dao/AdministradorDAO.java
@@ -2,7 +2,117 @@ package br.com.bibliotecaltv.dao;
 
 import org.springframework.stereotype.Repository;
 
+import br.com.bibliotecaltv.controller.javabeans.Administrador;
+import br.com.bibliotecaltv.controller.javabeans.Aluno;
+import br.com.bibliotecaltv.controller.javabeans.Emprestimo;
+import br.com.bibliotecaltv.controller.javabeans.Emprestimo_Sesc;
+import br.com.bibliotecaltv.controller.javabeans.Genero;
+import br.com.bibliotecaltv.controller.javabeans.Livro;
+import br.com.bibliotecaltv.controller.javabeans.Monitores;
+import br.com.bibliotecaltv.controller.javabeans.Professor;
+import br.com.bibliotecaltv.controller.javabeans.Turma;
+
 @Repository
 public class AdministradorDAO {
+
+
+	//Métodos de excluir dados
+	public void excluir(Aluno aluno) {
+		try {
+
+		} catch (Exception e) {
+
+		} finally {
+
+		}
+	}
+
+	public void excluir(Professor professor) {
+		try {
+
+		} catch (Exception e) {
+
+		} finally {
+
+		}
+	}
+
+	public void excluir(Monitores monitores){
+		try {
+
+		} catch (Exception e) {
+
+		} finally {
+
+		}
+
+	}
+
+	public void excluir(Administrador administrador){
+		try {
+
+		} catch (Exception e) {
+
+		} finally {
+
+		}
+
+	}
 	
+	public void excluir(Emprestimo emprestimo ){
+		try {
+			
+		} catch (Exception e) {
+			
+		} finally {
+			
+		}
+		
+	}
+	
+	public void excluir(Emprestimo_Sesc emprestimo_sesc){
+		try {
+			
+		} catch (Exception e) {
+			
+		} finally {
+			
+		}
+		
+	}
+	
+	public void excluir(Genero genero){
+		try {
+			
+		} catch (Exception e) {
+			
+		} finally {
+			
+		}
+		
+	}
+	
+	public void excluir(Livro livro){
+		try {
+			
+		} catch (Exception e) {
+			
+		} finally {
+			
+		}
+		
+	}
+	
+	public void excluir(Turma turma){
+		try {
+			
+		} catch (Exception e) {
+			
+		} finally {
+			
+		}
+		
+	}
+	
+	//Fim dos métodos de excluir dados
 }
