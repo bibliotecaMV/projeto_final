9a64a5537f7fd091ccd2bf3d48b61cb12ae85751
Pedro Lucas <pedro.matos@MVFOR.local>
2017-09-13 15:02:01 -0300

    Colocando o Autowired nas controllers

--- src/main/java/br/com/bibliotecaltv/controller/MonitorController.java
+++ src/main/java/br/com/bibliotecaltv/controller/MonitorController.java
@@ -2,7 +2,12 @@ package br.com.bibliotecaltv.controller;
 
 import org.springframework.stereotype.Controller;
 
+import br.com.bibliotecaltv.dao.MonitoresDAO;
+
 @Controller
 public class MonitorController {
-
+	private MonitoresDAO dao;
+	public MonitorController(MonitoresDAO dao){
+		this.dao = dao;
+	}
 }
