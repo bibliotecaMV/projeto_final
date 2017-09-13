e0f8a857f72b1916cd314f94b5b5fa04d984d4bc
Carla Valéria <carla_empresa@outlook.com>
2017-09-13 17:12:56 -0300

    colocando métodos excluir nas entidades do AdministradorDAO

--- src/main/java/br/com/bibliotecaltv/dao/AdministradorDAO.java
+++ src/main/java/br/com/bibliotecaltv/dao/AdministradorDAO.java
@@ -1,5 +1,7 @@
 package br.com.bibliotecaltv.dao;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 
 import br.com.bibliotecaltv.controller.javabeans.Administrador;
@@ -11,6 +13,7 @@ import br.com.bibliotecaltv.controller.javabeans.Livro;
 import br.com.bibliotecaltv.controller.javabeans.Monitores;
 import br.com.bibliotecaltv.controller.javabeans.Professor;
 import br.com.bibliotecaltv.controller.javabeans.Turma;
+import br.com.bibliotecaltv.sessaoHibernate.HibernateUtil;
 
 @Repository
 public class AdministradorDAO {
@@ -18,101 +21,164 @@ public class AdministradorDAO {
 
 	//Métodos de excluir dados
 	public void excluir(Aluno aluno) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction transaction = null;
+
 		try {
-
+			transaction = session.beginTransaction();
+			session.delete(aluno);
+			transaction.commit();
 		} catch (Exception e) {
-
+			if(transaction != null) {
+			transaction.rollback();
+			}
 		} finally {
-
+			session.close();
 		}
 	}
 
 	public void excluir(Professor professor) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction transaction = null;
+
 		try {
-
+			transaction = session.beginTransaction();
+			session.delete(professor);
+			transaction.commit();
 		} catch (Exception e) {
-
+			if(transaction != null) {
+			transaction.rollback();
+			}
 		} finally {
-
+			session.close();
 		}
 	}
 
 	public void excluir(Monitores monitores){
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction transaction = null;
+
 		try {
-
+			transaction = session.beginTransaction();
+			session.delete(monitores);
+			transaction.commit();
 		} catch (Exception e) {
-
+			if(transaction != null) {
+			transaction.rollback();
+			}
 		} finally {
-
+			session.close();
 		}
 
 	}
 
 	public void excluir(Administrador administrador){
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction transaction = null;
+
 		try {
-
+			transaction = session.beginTransaction();
+			session.delete(administrador);
+			transaction.commit();
 		} catch (Exception e) {
-
+			if(transaction != null) {
+				transaction.rollback();
+			}
 		} finally {
-
+			session.close();
 		}
 
 	}
-	
+
 	public void excluir(Emprestimo emprestimo ){
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction transaction = null;
+
 		try {
-			
+			transaction = session.beginTransaction();
+			session.delete(emprestimo);
+			transaction.commit();
 		} catch (Exception e) {
-			
+			if(transaction != null) {
+				transaction.rollback();
+			}
 		} finally {
-			
+			session.close();
 		}
-		
+
 	}
-	
+
 	public void excluir(Emprestimo_Sesc emprestimo_sesc){
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction transaction = null;
+
 		try {
-			
+			transaction = session.beginTransaction();
+			session.delete(emprestimo_sesc);
+			transaction.commit();
 		} catch (Exception e) {
-			
+			if(transaction != null) {
+				transaction.rollback();
+			}
 		} finally {
-			
+			session.close();
 		}
-		
+
 	}
-	
+
 	public void excluir(Genero genero){
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction transaction = null;
+
 		try {
-			
+			transaction = session.beginTransaction();
+			session.delete(genero);
+			transaction.commit();
 		} catch (Exception e) {
-			
+			if(transaction != null) {
+				transaction.rollback();
+			}
 		} finally {
-			
+			session.close();
 		}
-		
+
 	}
-	
+
 	public void excluir(Livro livro){
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction transaction = null;
+
 		try {
-			
+			transaction = session.beginTransaction();
+			session.delete(livro);
+			transaction.commit();
 		} catch (Exception e) {
-			
+			if(transaction != null) {
+				transaction.rollback();
+			}
 		} finally {
-			
+			session.close();
 		}
-		
+
 	}
-	
+
 	public void excluir(Turma turma){
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction transaction = null;
+
 		try {
-			
+			transaction = session.beginTransaction();
+			session.delete(turma);
+			transaction.commit();
 		} catch (Exception e) {
-			
+			if(transaction != null) {
+				transaction.rollback();
+			}
 		} finally {
-			
+			session.close();
 		}
-		
+
 	}
-	
+
 	//Fim dos métodos de excluir dados
 }
