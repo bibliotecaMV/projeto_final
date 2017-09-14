808437a08132ee9414997c2abe94b19c0f2693d6
Pedro Lucas <pedro.matos@MVFOR.local>
2017-09-14 16:44:20 -0300

    Cricao dos testes de buscarTurmaPorId e buscarIdTurma

---
+++ src/main/java/br/com/bibliotecaltv/teste/TesteBuscarGeneroPorId.java
@@ -0,0 +1,12 @@
+package br.com.bibliotecaltv.teste;
+
+import br.com.bibliotecaltv.controller.javabeans.Genero;
+import br.com.bibliotecaltv.dao.AdministradorDAO;
+
+public class TesteBuscarGeneroPorId {
+	public static void main(String[] args) {
+		AdministradorDAO dao = new AdministradorDAO();
+		Genero genero = dao.buscarGeneroPorId(1L);
+		System.out.println(genero.getDescricao());
+	}
+}
