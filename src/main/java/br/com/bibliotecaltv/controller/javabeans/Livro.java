573301a8af3c6fed5bb9d69f3d650579b7930ec2
Pedro Lucas <pedro.matos@MVFOR.local>
2017-09-15 15:47:22 -0300

    Tirando os namedquery's inuteis

--- src/main/java/br/com/bibliotecaltv/controller/javabeans/Livro.java
+++ src/main/java/br/com/bibliotecaltv/controller/javabeans/Livro.java
@@ -13,8 +13,6 @@ import javax.persistence.Table;
 @Table(name = "livros")
 @NamedQueries({
 	     @NamedQuery(name="Livro.listarLivro", query="SELECT livro FROM Livro livro"),
-	     @NamedQuery(name = "livros.buscarId", query = "select livro from Livro livro where livro.tombo = :tombo"),
-         @NamedQuery(name = "livros.buscarLivro", query = "select titulo from Livro livro where livro.titulo = :titulo"),
 	     @NamedQuery(name = "Livro.listarPorTombo", query = "select livro from Livro livro where tombo = :tombo")})
 public class Livro {
 	@Id
