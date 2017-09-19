93a7b583a7f3ca068108934293a3fcdc39e428e6
Carla Valéria <carla_empresa@outlook.com>
2017-09-19 17:57:25 -0300

    colocando mais turmas para inserir

--- src/main/java/br/com/bibliotecaltv/teste/TesteInserirTurma.java
+++ src/main/java/br/com/bibliotecaltv/teste/TesteInserirTurma.java
@@ -11,9 +11,40 @@ public class TesteInserirTurma {
 		Turma turma = new Turma();
 		turma.setNome("3A");
 		dao.adicionarTurma(turma);
-		System.out.println("Adicionando turma");
+		System.out.println("Adicionando 3A");
 		
-
+		turma.setNome("2A");
+		dao.adicionarTurma(turma);
+		System.out.println("Adicionando 2A");
+		
+		turma.setNome("1A");
+		dao.adicionarTurma(turma);
+		System.out.println("Adicionando 1A");
+		
+		turma.setNome("3B");
+		dao.adicionarTurma(turma);
+		System.out.println("Adicionando 3B");
+		
+		turma.setNome("2B");
+		dao.adicionarTurma(turma);
+		System.out.println("Adicionando 2B");
+		
+		turma.setNome("1B");
+		dao.adicionarTurma(turma);
+		System.out.println("Adicionando 1B");
+		
+		turma.setNome("3C");
+		dao.adicionarTurma(turma);
+		System.out.println("Adicionando 3C");
+		
+		turma.setNome("2C");
+		dao.adicionarTurma(turma);
+		System.out.println("Adicionando 2C");
+		
+		turma.setNome("1C");
+		dao.adicionarTurma(turma);
+		System.out.println("Adicionando 1C");
+		
 	}
 
 }
