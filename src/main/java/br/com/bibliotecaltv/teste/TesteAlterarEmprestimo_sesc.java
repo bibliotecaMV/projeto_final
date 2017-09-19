d9bbb13b079bf34703f502c1a90308fb6a454361
MaykLucas <mayklucas2015@gmail.com>
2017-09-19 15:24:26 -0300

    teste alterar

---
+++ src/main/java/br/com/bibliotecaltv/teste/TesteAlterarEmprestimo_sesc.java
@@ -0,0 +1,14 @@
+package br.com.bibliotecaltv.teste;
+
+import br.com.bibliotecaltv.controller.javabeans.Emprestimo_Sesc;
+import br.com.bibliotecaltv.dao.AdministradorDAO;
+
+public class TesteAlterarEmprestimo_sesc {
+
+	public static void main(String[] args) {
+		AdministradorDAO dao = new AdministradorDAO();
+		Emprestimo_Sesc emprestimo_sesc;
+
+	}
+
+}
