d1db9a484e79b9927631b51989abcaa1800c63cf
MaykLucas <mayklucas2015@gmail.com>
2017-09-20 14:07:29 -0300

    debug da classes

--- src/main/java/br/com/bibliotecaltv/teste/TesteExcluirAluno.java
+++ src/main/java/br/com/bibliotecaltv/teste/TesteExcluirAluno.java
@@ -6,7 +6,7 @@ import br.com.bibliotecaltv.dao.AdministradorDAO;
 public class TesteExcluirAluno {
 	public static void main(String[] args) {
 		AdministradorDAO dao = new AdministradorDAO();
-		Long id_aluno = dao.buscarIdAluno("Pedro Lucas");
+		Long id_aluno = dao.buscarIdAluno("Renato Richard");
 		Aluno aluno = dao.buscarAlunoPorId(id_aluno);
 		dao.excluirAluno(aluno);
 		System.out.println("Exclui você otário!!!");
