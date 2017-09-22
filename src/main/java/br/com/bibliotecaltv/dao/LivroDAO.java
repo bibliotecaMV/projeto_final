db3f472f870c034f3f13eedb3fc6d6b5c088116c
Pedro Lucas <pedro99lucasom@gmail.com>
2017-09-22 14:38:02 -0300

    Teste de listar por tombo concluido

--- src/main/java/br/com/bibliotecaltv/dao/LivroDAO.java
+++ src/main/java/br/com/bibliotecaltv/dao/LivroDAO.java
@@ -1,7 +1,24 @@
 package br.com.bibliotecaltv.dao;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 import br.com.bibliotecaltv.controller.javabeans.Livro;
+import br.com.bibliotecaltv.sessaoHibernate.HibernateUtil;
 
 public class LivroDAO extends GenericDAO<Livro, Long> {
-	
+	public Livro listarPorId(Class<Livro> classe, String pk) throws Exception{
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction transaction = null;
+		try {
+			transaction = session.beginTransaction();
+			Livro livro = (Livro) session.load(classe, pk);
+			session.flush();
+			transaction.commit();
+			return  livro;
+		}catch(RuntimeException e){
+			throw e;
+		}
+	}
+
 }
--- src/main/java/br/com/bibliotecaltv/teste/listarPorId/TesteBuscarLivroPorTombo.java
+++ src/main/java/br/com/bibliotecaltv/teste/listarPorId/TesteBuscarLivroPorTombo.java
@@ -1,12 +1,12 @@
 package br.com.bibliotecaltv.teste.listarPorId;
 
 import br.com.bibliotecaltv.controller.javabeans.Livro;
-import br.com.bibliotecaltv.dao.AdministradorDAO;
+import br.com.bibliotecaltv.dao.LivroDAO;
 
 public class TesteBuscarLivroPorTombo {
-	public static void main(String[] args) {
-		AdministradorDAO dao = new AdministradorDAO();
-		Livro livro = dao.buscarLivroPorTombo("3455545");
+	public static void main(String[] args) throws Exception {
+		LivroDAO dao = new LivroDAO();
+		Livro livro = dao.listarPorId(Livro.class,"3455545");
 		System.out.println(livro.getTitulo());
 	}
 }
